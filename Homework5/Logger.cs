2374f806651f69a8439d6ee2a15ebf55e4d59874
Maksym Yankovskyi <maks.yankovskiy27@gmail.com>
2023-03-29 15:46:53 +0300

    add LoggerRecord class with properties DateTime and change methods in class Logger

--- Homework5/Logger.cs
+++ Homework5/Logger.cs
@@ -10,6 +10,7 @@ namespace Homework5
     {
         private static Logger? _instance;
         private readonly List<string> _logs = new List<string>();
+        private readonly LoggerRecord _loggerRecord = new LoggerRecord();
 
         private Logger()
         {
@@ -27,20 +28,20 @@ namespace Homework5
 
         public void LoggerError(string text)
         {
-            _logs.Add($"{DateTime.Now}; {LoggerType.Error}: {text}");
-            Console.WriteLine($"{DateTime.Now}; {LoggerType.Error}: {text}");
+            _logs.Add($"{_loggerRecord.Date}; {LoggerType.Error}: {text}");
+            Console.WriteLine($"{_loggerRecord.Date}; {LoggerType.Error}: {text}");
         }
 
         public void LoggerInfo(string text)
         {
-            _logs.Add($"{DateTime.Now}; {LoggerType.Info}: {text}");
-            Console.WriteLine($"{DateTime.Now}; {LoggerType.Info}: {text}");
+            _logs.Add($"{_loggerRecord.Date}; {LoggerType.Info}: {text}");
+            Console.WriteLine($"{_loggerRecord.Date}; {LoggerType.Info}: {text}");
         }
 
         public void LoggerWarning(string text)
         {
-            _logs.Add($"{DateTime.Now}; {LoggerType.Warning}: {text}");
-            Console.WriteLine($"{DateTime.Now}; {LoggerType.Warning}: {text}");
+            _logs.Add($"{_loggerRecord.Date}; {LoggerType.Warning}: {text}");
+            Console.WriteLine($"{_loggerRecord.Date}; {LoggerType.Warning}: {text}");
         }
 
         public string GetLogs()
---
+++ Homework5/LoggerRecord.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework5
+{
+    public class LoggerRecord
+    {
+        public DateTime Date
+        {
+            get { return DateTime.Now; }
+        }
+    }
+}
