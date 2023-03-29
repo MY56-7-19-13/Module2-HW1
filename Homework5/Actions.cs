867ebbbcdbd588c00633d8368093b08a964829ac
Maksym Yankovskyi <maks.yankovskiy27@gmail.com>
2023-03-29 15:22:35 +0300

    add full homework5

---
+++ Homework5/Actions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework5
+{
+    public class Actions
+    {
+        public Result StartMethod()
+        {
+            Logger.GetInstance().LoggerInfo("Start method");
+            return new Result { Status = true };
+        }
+
+        public Result SkippedLogicInMethod()
+        {
+            Logger.GetInstance().LoggerWarning("Skipped logic in method");
+            return new Result { Status = true };
+        }
+
+        public Result BrokeMethod()
+        {
+            Result result = new Result() { Status = false, ErrorMessage = "I broke a logic" };
+            Logger.GetInstance().LoggerError($"Action failed by a reason: {result.ErrorMessage}");
+            return result;
+        }
+    }
+}
---
+++ Homework5/Logger.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework5
+{
+    public class Logger
+    {
+        private static Logger? _instance;
+        private readonly List<string> _logs = new List<string>();
+
+        private Logger()
+        {
+        }
+
+        public static Logger GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Logger();
+            }
+
+            return _instance;
+        }
+
+        public void LoggerError(string text)
+        {
+            _logs.Add($"{DateTime.Now}; {LoggerType.Error}: {text}");
+            Console.WriteLine($"{DateTime.Now}; {LoggerType.Error}: {text}");
+        }
+
+        public void LoggerInfo(string text)
+        {
+            _logs.Add($"{DateTime.Now}; {LoggerType.Info}: {text}");
+            Console.WriteLine($"{DateTime.Now}; {LoggerType.Info}: {text}");
+        }
+
+        public void LoggerWarning(string text)
+        {
+            _logs.Add($"{DateTime.Now}; {LoggerType.Warning}: {text}");
+            Console.WriteLine($"{DateTime.Now}; {LoggerType.Warning}: {text}");
+        }
+
+        public string GetLogs()
+        {
+            return string.Join(Environment.NewLine, _logs);
+        }
+    }
+}
